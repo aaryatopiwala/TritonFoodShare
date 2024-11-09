ba0566aacd9bca9bb413210de8bfbc68f1c55f36
Connor Fan <ConnortheFan@gmail.com>
2024-11-08 21:14:40 -0800

    Created backend database with SQLite3. Endpoints not linked with client-side

---
+++ my-app/server/src/createTable.ts
@@ -0,0 +1,24 @@
+import sqlite3 from "sqlite3";
+import { open } from "sqlite";
+
+const initDB = async () => {
+    // Open the database connection
+    const db = await open({
+        filename: "database.sqlite",
+      driver: sqlite3.Database,
+    });
+    // Create a "budget" table if it doesn't exist
+    await db.exec(`
+        CREATE TABLE IF NOT EXISTS foodEvents (
+        id TEXT PRIMARY KEY,
+        orgName TEXT,
+        foodName TEXT,
+        quantity TEXT,
+        location TEXT,
+        description TEXT
+        );
+    `);
+    return db;
+};
+
+export default initDB;
---
+++ my-app/server/src/types.ts
@@ -0,0 +1,8 @@
+export interface FoodEvent {
+    id: string;
+    orgName: string;
+    foodName: string;
+    quantity: string;
+    location: string;
+    description: string;
+}
