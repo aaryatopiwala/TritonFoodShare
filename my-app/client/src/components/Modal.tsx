899207aea29c17b432aa78ed5e1771e506a0c5cd
aaryatopiwala <75400069+aaryatopiwala@users.noreply.github.com>
2024-11-15 23:06:06 -0800

    Frontend UI for Edit Feature + tests

---
+++ my-app/client/src/components/Modal.tsx
@@ -0,0 +1,62 @@
+import React, { useRef, useEffect, useState } from "react";
+import './EditEventModal.css'; // Import the CSS file
+
+
+interface ModalProps {
+  isOpen: boolean;
+  hasCloseBtn?: boolean;
+  onClose?: () => void;
+  children: React.ReactNode;
+};
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  hasCloseBtn = true,
+  onClose,
+  children
+}) => {
+  const [isModalOpen, setModalOpen] = useState(isOpen);
+  const modalRef = useRef<HTMLDialogElement | null>(null);
+
+  const handleCloseModal = () => {
+    if (onClose) {
+      onClose();
+    }
+    setModalOpen(false);
+  };
+
+  const handleKeyDown = (event: React.KeyboardEvent<HTMLDialogElement>) => {
+    if (event.key === "Escape") {
+      handleCloseModal();
+    }
+  };
+
+  useEffect(() => {
+    setModalOpen(isOpen);
+  }, [isOpen]);
+
+  useEffect(() => {
+    const modalElement = modalRef.current;
+
+    if (modalElement) {
+      if (isModalOpen) {
+        modalElement.showModal();
+      } else {
+        modalElement.close();
+      }
+    }
+  }, [isModalOpen]);
+
+  return (
+    <dialog ref={modalRef} onKeyDown={handleKeyDown} className="modal-content wide-modal">
+      {hasCloseBtn && (
+        <button className="modal-close-btn" onClick={handleCloseModal}>
+          Close
+        </button>
+      )}
+      {children}
+    </dialog>
+  );
+};
+
+export default Modal;
