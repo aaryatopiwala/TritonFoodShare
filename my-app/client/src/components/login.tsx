be93875f23377f7e9794e536ea12ff3c57bf4799
chrissngn <123513732+chrissngn@users.noreply.github.com>
2024-11-23 23:16:15 -0800

    login/signup page ui update

--- my-app/client/src/components/login.tsx
+++ my-app/client/src/components/login.tsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import './login.css'; 
-import NavBar from './NavBar';
+import React from "react";
+import "./login.css";
 
 const LoginPage = () => {
     return (
-        <div className="App">
-    {/* <NavBar /> Navbar stays fixed or at the top */}
-    <div className="login-page">
-        <div className="login-container">
-            <div className="login-box">
-                <h1>Login!</h1>
-                <form>
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" placeholder="Enter your email" required />
+        <div className="login-page">
+            <div className="login-container">
+                <div className="login-box">
+                    <div className="login-logo">
+                        <img src="https://png.pngtree.com/png-vector/20220708/ourmid/pngtree-fast-food-logo-png-image_5763171.png" alt="login logo" />
+                    </div>
+                    <h1>Log in</h1>
+                    <form>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" placeholder="Enter your email" required />
 
-                    <label htmlFor="password">Password</label>
-                    <input type="password" id="password" placeholder="Enter your password" required />
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" placeholder="Enter your password" required />
 
-                    <button type="submit">Log in</button>
-                </form>
-                <p>Don’t have an account? <a href="/signup">Sign up here!</a></p>
+                        <button type="submit">Log in</button>
+                    </form>
+                    <p>
+                        Don’t have an account? <a href="/signup">Sign up here!</a>
+                    </p>
+                </div>
             </div>
         </div>
-    </div>
-</div>
     );
 };
 
