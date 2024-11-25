15b01f98313f8d562d9fe2ee3fd71d1a53c58543
aaryatopiwala <75400069+aaryatopiwala@users.noreply.github.com>
2024-11-24 23:21:50 -0800

    Revert CSS changes from pre-Turso merge

--- my-app/client/src/components/EventSubmissionForm.tsx
+++ my-app/client/src/components/EventSubmissionForm.tsx
@@ -18,7 +18,7 @@ const EventSubmissionForm = () => {
   const navigate = useNavigate();
 
   // Location tags
-  const [selectedOptionLoction, setSelectedOptionLocation] = useState<SingleValue<OptionType>>(null);
+  const [selectedOptionLocation, setSelectedOptionLocation] = useState<SingleValue<OptionType>>(null);
   const locationOptions = [
     { value: 'cseBuilding', label: 'CSE Building' },
     { value: 'WLH', label: 'Warren Lecture Hall' },
@@ -58,7 +58,7 @@ const EventSubmissionForm = () => {
         foodName,
         quantity: parseInt(quantity),
         locationDescription: location,
-        bigLocation: selectedOptionLoction?.value || '',
+        bigLocation: selectedOptionLocation?.value || '',
         diet: selectedOptionDiet?.value || '',
     };
 
@@ -101,92 +101,96 @@ const EventSubmissionForm = () => {
     navigate("/eventdisplay");
   };
 
-
   return (
-    <div className="custom-container">
-      <form onSubmit={onSubmit}>
-        <div style={{ marginBottom: '24px' }}>
-          <label htmlFor="orgName" className="custom-label">Organization Name</label>
-          <input
-            required
-            type="text"
-            placeholder="Who are you?"
-            data-testid="orgName"
-            id="orgName"
-            value={orgName}
-            onChange={(event) => setOrgName(event.target.value)}
-            className="custom-input"
-          />
-        </div>
-        <div style={{ marginBottom: '24px' }}>
-          <label htmlFor="foodName" className="custom-label">Food Name</label>
-          <input
-            required
-            type="text"
-            placeholder="chipotle"
-            data-testid="foodName"
-            id="foodName"
-            value={foodName}
-            onChange={(event) => setFoodName(event.target.value)}
-            className="custom-input"
-          />
-        </div>
-        <div style={{ marginBottom: '24px' }}>
-          <label htmlFor="quantity" className="custom-label">Quantity</label>
-          <input
-            required
-            type="number" // Use type="number" for numeric input
-            placeholder="23"
-            data-testid="quantity"
-            id="quantity"
-            value={quantity}
-            onChange={(event) => setQuantity(event.target.value)}
-            className="custom-input"
-          />
-        </div>
-        <div style={{ marginBottom: '24px' }}>
-          <label htmlFor="locationDescription" className="custom-label">Location Description</label>
-          <textarea
-            required
-            placeholder="CSE building"
-            data-testid="locationDescription"
-            id="locationDescription"
-            value={location}
-            onChange={(event) => setLocation(event.target.value)}
-            className="custom-input custom-textarea"
-          />
-        </div>
-        <div style={{ marginBottom: '24px' }}>
-          <label htmlFor="BigLocation" className="custom-label">Search for location</label>
-          <Select
-            id="BigLocation"
-            value={selectedOptionLoction}
-            onChange={handleChangeLocation}
-            options={locationOptions}
-            placeholder="Search for your location..."
-            isSearchable
-          />
-        </div>
-        <div style={{ marginBottom: '24px' }}>
-          <label htmlFor="diet" className="custom-label">Dietary tags</label>
-          <Select
-            id="diet"
-            value={selectedOptionDiet}
-            onChange={handleChangeDiet}
-            options={dietOptions}
-            placeholder="Search for tags..."
-            isSearchable
-          />
-        </div>
-        <button type="submit" style={{ width: '100%', height: '40px', borderRadius: '8px', backgroundColor: '#4CAF50', color: 'white', fontSize: '16px', border: 'none' }}>
-          Submit
-        </button>
-      </form>
-      {showConfirmation && (
+    <div className="submission-container">
+      <div className="form-section">
+        <form onSubmit={onSubmit}>
+          {/* Form Fields */}
+          <div className="form-group">
+            <label htmlFor="orgName" className="label">Organization Name</label>
+            <input
+              required
+              type="text"
+              placeholder="Who are you?"
+              data-testid="orgName"
+              id="orgName"
+              value={orgName}
+              onChange={(event) => setOrgName(event.target.value)}
+              className="custom-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="foodName" className="label">Food Name</label>
+            <input
+              required
+              type="text"
+              placeholder="What would you like to share? Ex. Chipotle Burritos"
+              data-testid="foodName"
+              id="foodName"
+              value={foodName}
+              onChange={(event) => setFoodName(event.target.value)}
+              className="custom-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="quantity" className="label">Quantity</label>
+            <input
+              required
+              type="number"
+              placeholder="Enter the number of servings that are available"
+              data-testid="quantity"
+              id="quantity"
+              value={quantity}
+              onChange={(event) => setQuantity(event.target.value)}
+              className="custom-input"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="location" className="label">Location Description</label>
+            <textarea
+              required
+              placeholder="Please be descriptive as possible (ex. building, room number, directions to get there, etc.)"
+              data-testid="location"
+              id="location"
+              value={location}
+              onChange={(event) => setLocation(event.target.value)}
+              className="custom-textarea"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="BigLocation" className="label">Search for location</label>
+            <Select
+              id="BigLocation"
+              value={selectedOptionLocation}
+              onChange={handleChangeLocation}
+              options={locationOptions}
+              placeholder="Search for your location..."
+              isSearchable
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="diet" className="label">Dietary Tags</label>
+            <Select
+              id="diet"
+              value={selectedOptionDiet}
+              onChange={handleChangeDiet}
+              options={dietOptions}
+              placeholder="Search for tags..."
+              isSearchable
+            />
+          </div>
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+        {showConfirmation && (
         <div className="confirmation-message">
             🎉 Your event has been successfully submitted!
         </div>
-      )}
+        )}
+      </div>
+
+      {/* My Events Section */}
       <div className="events-section">
         {foodEvents.length > 0 ? (
           <>
@@ -212,12 +216,15 @@ const EventSubmissionForm = () => {
             <p>Submit a form to view it here!</p>
           </div>
         )}
-      </div>
-      <button className="view-active-events-button" onClick={handleViewActiveEvents}>
+        <button className="view-active-events-button" onClick={handleViewActiveEvents}>
           View Active Events
-      </button>
+        </button>
+      </div>
+
+
+
     </div>
-    
+
   );
 };
 
