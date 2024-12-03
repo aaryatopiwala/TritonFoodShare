a32554479306d4acd10d5656918c581821aefd61
chrissngn <123513732+chrissngn@users.noreply.github.com>
2024-12-02 22:56:02 -0800

    Adding graphics to homepage, adjusting UI

--- my-app/client/src/views/Home.tsx
+++ my-app/client/src/views/Home.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';  // Import Link from react-router-dom
 import "./Home.css"
+import img1 from './img1.png';
+import img2 from './img2.png';
+
 const HomePage = () => {
     return (
        <div className = "main-container">
@@ -42,16 +45,13 @@ const HomePage = () => {
                     <div className = "user-options">
                         <div className = "org-box">
                         <h4>For Organizations:</h4>
-                            <p>Getting started is easy! Simply create an account for your organization and begin submitting details about any leftover food you’d like to share. Include information such as the type of food, quantity, location, and any dietary details. You can freely edit or close your submissions at any time to keep the information up-to-date.
-
-                            [insert place holder image, this will be replaced with an image with arrows labels explaining what the tools are]</p>
-                        </div>
+                            <p>Getting started is easy! Simply create an account for your organization and begin submitting details about any leftover food you’d like to share. Include information such as the type of food, quantity, location, and any dietary details. You can freely edit or close your submissions at any time to keep the information up-to-date.</p>
+                            <img src={img2} alt="Description for img1" />                        </div>
                         <div className = "student-box">
                         <h4>For Students:</h4>
-                                <p>Sign up for an account and start reserving food from events today! Browse active submissions to see all the details shared by organizations, including food type, quantity, and location. If you change your plans, you can easily unreserve your spot with just a click.
-
-                                [insert place holder image, this will be replaced with an image with arrows labels explaining what the tools are]</p>
-                        </div>
+                                <p>Sign up for an account and start reserving food from events today! Browse active submissions to see all the details shared by organizations, including food type, quantity, and location. If you change your plans, you can easily unreserve your spot with just a click.</p>
+                                <img src={img1} alt="Description for img2" />
+                                </div>
                     </div>
                 </div>
             </div>
